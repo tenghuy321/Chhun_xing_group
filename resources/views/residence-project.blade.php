ae2e42bd6d777757439c83b26eafefe4971656e2
Tenghuy <tenghuyly35@gmailcom>
2025-06-09 22:31:28 +0700

    new

--- resources/views/residence-project.blade.php
+++ resources/views/residence-project.blade.php
@@ -36,7 +36,7 @@
         <h1 class="text-gradient text-[20px] md:text-[25px] mb-10 font-[600] max-w-[130px] sm:max-w-full">
             {{ $projectExperince_title->title[app()->getLocale()] }}</h1>
 
-        <div class="relative">
+        {{-- <div class="relative">
             <div class="absolute -top-[15%] right-0 z-10 flex gap-2 p-2">
                 <button
                     class="custom-prev bg-[#966927] p-0 text-white w-10 h-10 rounded-full flex items-center justify-center">
@@ -89,6 +89,43 @@
                     @endforelse
                 </div>
             </div>
+        </div> --}}
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-6">
+            @forelse ($projectExperince as $index => $experince)
+                @php
+                    $images = json_decode($experince->image, true) ?? [];
+                @endphp
+                <div class="flex flex-col gap-4 shadow-md">
+                    <div class="swiper ProjectExperinceSwiper w-full h-full">
+                        <div class="swiper-wrapper">
+                            @foreach ($images as $img)
+                                <div class="swiper-slide w-full h-full">
+                                    <img src="{{ asset($img) }}" loading="lazy"
+                                        alt="{{ $experince->title[app()->getLocale()] }}"
+                                        class="w-full h-[300px] object-cover cursor-pointer"
+                                        onclick="openLightbox('{{ asset($img) }}', '{{ $experince->title[app()->getLocale()] }}')">
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="swiper-pagination"></div>
+                    </div>
+                    <div class="w-full p-2">
+                        <div class="min-h-[60px] flex items-start">
+                            <h1 class="text-[14px] md:text-[16px] font-[600] text-gradient">
+                                {{ $experince->title[app()->getLocale()] }}
+                            </h1>
+                        </div>
+                        <div
+                            class="prose prose-p:m-0 prose-p:text-[#000] prose-h1:text-[#000] prose-h2:text-[#000] prose-h3:text-[#000] prose-ul:text-[#000] prose-ol:text-[#000] prose-ul:marker:text-[#000]">
+                            {!! $experince->content[app()->getLocale()] !!}
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <div class="md:col-span-12 text-center py-10">
+                    <p>No projects found</p>
+                </div>
+            @endforelse
         </div>
     </div>
 
